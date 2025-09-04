0d61843391ac7de0ce343ebe46ee9f0242b7fc07
madesha2007 <gpta157madesha@gmail.com>
2025-09-04 09:54:21 +0530

    Add files via upload

---
+++ codacy.py
@@ -0,0 +1,17 @@
+a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+add=ab
+sub=a-b
+mul=a*b
+div=a/b
+fd=a//b
+mod=a%b
+exp=a**b
+print("add:",a+b)
+print("sub:",a-b)
+print("mul:",a*b)
+print("div:",a/b)
+print("fd:",a//b)
+print("mod:",a%b)
+print("exp:",a**b)
+
